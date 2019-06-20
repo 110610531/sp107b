57debe83b8072d2d251735b807f40e64911130d3
110610531 <36975689+110610531@users.noreply.github.com>
2019-06-20 17:41:24 +0800

    Add files via upload

--- compiler.c
+++ compiler.c
@@ -40,14 +40,14 @@ char *skip(char *set) {
 // F = (E) | Number | Id
 int F() {
   int f;
-  if (isNext("(")) { // '(' E ')'
+  if (isNext("(")) { // '(' E ')'//有括號讀括號,但是if已經把括號除掉了,所以不用讀
     next(); // (
     f = E();
     next(); // )
   } else { // Number | Id
-    f = nextTemp();
-    char *item = next();
-    emit("t%d = %s\n", f, item);
+    f = nextTemp();//這個是t前面的數字
+    char *item = next();//這個事t等於的東西
+    emit("t%d = %s\n", f, item);//把f,item所算出來的東西丟到這個算式
   }
   return f;
 }
@@ -56,10 +56,10 @@ int F() {
 int E() {
   int i1 = F();
   while (isNext("+ - * / & | ! < > =  == >= <=")) {
-    char *op = next();
+    char *op = next();//*op是上一行的運算符號
     int i2 = E();
-    int i = nextTemp();
-    emit("t%d = t%d %s t%d\n", i, i1, op, i2);
+    int i = nextTemp();//nextTemp要加１,因為上一個數字已經用過了
+    emit("t%d = t%d %s t%d\n", i, i1, op, i2);//把之前所算出來的直帶進去
     i1 = i;
   }
   return i1;
@@ -99,14 +99,12 @@ void IF(){
   skip(")");
   emit("ifnot t%d goto L%d\n", e, elseLabel);
   STMT();
-  emit("if t%d goto L%d\n", e, endifLabel);
   if(isNext("else")){
-    emit("L%d",elseLabel);
-    skip("(");
+    emit("(L%d)\n",elseLabel);
+    skip("else");
     STMT();
-    skip(")");
   }
-  emit("L%d", endifLabel);
+  emit("(L%d)\n", endifLabel);
 }
 
 void STMT() {
